cf186acc87d9bbcd614fdf12332ab8eb105e3c8d
Matheus Ferreira <matheusfdantas03@gmail.com>
2023-01-23 14:05:28 -0300

    Component: Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Transactions() {
   return (
     <>
       <Header />
-      <h1>Trasactions</h1>
+      <Summary />
     </>
   )
 }
